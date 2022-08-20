30202d9aaee40e4ad08cc639b33b6835ae94ad3b
hardikothiya <hardikothiya@gmail.com>
2022-08-20 11:34:01 +0530

    post views added

--- api/views.py
+++ api/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 from rest_framework import generics
 from api import serializers
 from django.contrib.auth.models import User
+from api.models import Post
+from rest_framework import permissions
+from api.permissions import IsOwnerOrReadOnly
 
 
 class UserList(generics.ListAPIView):
@@ -14,3 +17,20 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
